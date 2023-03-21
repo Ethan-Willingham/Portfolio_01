90d76e8ed23476219382da2890dc6b7cf816ed13
Ethan Willingham <82549015+Ethan-Willingham@users.noreply.github.com>
2023-03-20 19:19:49 -0500

    made world larger, added ores and top layer

--- game.js
+++ game.js
@@ -22,23 +22,67 @@ class Block {
     this.y = y;
     this.size = 20;
     this.type = type;
+    
   }
 
   draw() {
-    ctx.fillStyle = this.type === "dirt" ? "saddlebrown" : "gray";
+    switch (this.type) {
+      case "sky":
+        ctx.fillStyle = "skyblue";
+        break;
+      case "grass":
+        ctx.fillStyle = "green";
+        break;
+      case "dirt":
+        ctx.fillStyle = "saddlebrown";
+        break;
+      case "stone":
+        ctx.fillStyle = "gray";
+        break;
+      default:
+        ctx.fillStyle = "gold";
+    }
     ctx.fillRect(this.x, this.y, this.size, this.size);
   }
 }
 
 
 const world = [];
-const worldWidth = 1600;
-const worldHeight = 1200;
+const worldWidth = 3200;
+const worldHeight = 2400;
 
 function generateTerrain() {
   for (let x = 0; x < worldWidth; x += 20) {
     for (let y = 0; y < worldHeight; y += 20) {
-      const type = Math.random() < 0.8 ? "dirt" : "stone";
+      let type;
+
+      if (y < 80) {
+        type = "sky";
+      } else if (y < 100) {
+        type = "grass";
+      } else {
+        const randomValue = Math.random();
+        if (randomValue < 0.7) {
+          type = "dirt";
+        } else if (randomValue < 0.95) {
+          type = "stone";
+        } else {
+          const oreProbabilities = [
+            { type: "ore1", prob: 0.5 },
+            { type: "ore2", prob: 0.65 },
+            { type: "ore3", prob: 0.75 },
+            { type: "ore4", prob: 0.85 },
+            { type: "ore5", prob: 0.9 },
+            { type: "ore6", prob: 0.95 },
+            { type: "ore7", prob: 1 },
+          ];
+
+          const depthFactor = y / worldHeight;
+          const selectedOre = oreProbabilities.find(ore => depthFactor <= ore.prob);
+          type = selectedOre.type;
+        }
+      }
+
       const block = new Block(x, y, type);
       world.push(block);
     }
@@ -70,28 +114,33 @@ const miner = new Miner(canvas.width / 2, 0);
 function gameLoop() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+  camera.update();
+  ctx.save();
+  ctx.translate(-camera.x, -camera.y);
+
   world.forEach((block) => block.draw());
   miner.draw();
 
-  requestAnimationFrame(gameLoop);;
+  ctx.restore();
+  requestAnimationFrame(gameLoop);
 }
 
 gameLoop();
 
 document.addEventListener("keydown", (e) => {
-  const speed = 5;
+  const blockSize = 20;
   switch (e.key) {
     case "ArrowUp":
-      miner.y -= speed;
+      miner.y -= blockSize;
       break;
     case "ArrowDown":
-      miner.y += speed;
+      miner.y += blockSize;
       break;
     case "ArrowLeft":
-      miner.x -= speed;
+      miner.x -= blockSize;
       break;
     case "ArrowRight":
-      miner.x += speed;
+      miner.x += blockSize;
       break;
   }
 });
